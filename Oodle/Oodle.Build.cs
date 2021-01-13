b55c7d84ce67f999cdf93f8f3cf502acda659c4a
jashking <newzeadev@gmail.com>
2021-01-13 15:54:30 +0800

    auto copy oodle dll to engine binary folder

--- Oodle/Oodle.Build.cs
+++ Oodle/Oodle.Build.cs
@@ -1,6 +1,7 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class Oodle : ModuleRules
 {
@@ -14,5 +15,18 @@ public class Oodle : ModuleRules
 				"Core",
 			}
 		);
+
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			string DllName = "oo2core_8_win64.dll";
+			string ToPath = Path.Combine(EngineDirectory, "Binaries", "Win64", DllName);
+			string FromPath = Path.Combine(ModuleDirectory, "3rd", DllName);
+
+			if (!File.Exists(ToPath))
+			{
+				File.Copy(FromPath, ToPath, true);
+				System.Console.WriteLine(string.Format("Copy {0} from {1} to {2}", DllName, FromPath, ToPath));
+			}
+		}
 	}
 }
