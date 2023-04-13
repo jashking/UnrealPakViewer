7ecb78c0bccb45a222bd6b4d63cf7c74d76b1745
jashking <newzeadev@gmail.com>
2023-04-13 19:35:41 +0800

    update version

--- UnrealPakViewer.Target.cs
+++ UnrealPakViewer.Target.cs
@@ -31,6 +31,6 @@ public class UnrealPakViewerTarget : TargetRules
 
 		GlobalDefinitions.Add("NOINITCRASHREPORTER=1");
 		GlobalDefinitions.Add("WITH_CASE_PRESERVING_NAME=0");
-		GlobalDefinitions.Add(string.Format("UNREAL_PAK_VIEWER_VERSION=TEXT(\"{0}\")", "1.3"));
+		GlobalDefinitions.Add(string.Format("UNREAL_PAK_VIEWER_VERSION=TEXT(\"{0}\")", "1.4"));
 	}
 }
